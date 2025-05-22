2b1b9567c7a86ff8d978d293ea37f5f1a96b5a79
4ahlikNevmiruchi <viktor.yudakov.1104@gmail.com>
2025-05-22 16:13:29 +0300

    Remove build and .user from Git tracking

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,7 +0,0 @@
-set(__QT_DEPLOY_TARGET_gui_lib_FILE C:/Users/Viktor/Labs/PoliticalSim/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/src/gui/libgui_lib.a)
-set(__QT_DEPLOY_TARGET_gui_lib_TYPE STATIC_LIBRARY)
-set(__QT_DEPLOY_TARGET_src_lib_FILE C:/Users/Viktor/Labs/PoliticalSim/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/src/libsrc_lib.a)
-set(__QT_DEPLOY_TARGET_src_lib_TYPE STATIC_LIBRARY)
-set(__QT_DEPLOY_TARGET_PoliticalSim_FILE C:/Users/Viktor/Labs/PoliticalSim/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/PoliticalSim.exe)
-set(__QT_DEPLOY_TARGET_PoliticalSim_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PoliticalSim_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
